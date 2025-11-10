23addd8580336df4e1b0c2dd34695242634d8893
xhos <60789741+xhos@users.noreply.github.com>
2025-11-10 17:48:35 -0500

    fix key

--- configuration.nix
+++ configuration.nix
@@ -10,6 +10,8 @@
       80 # http
       443 # https
 
+      1080 # proxy
+
       25565 # minecraft
 
       2222 # ssh for minecraft
@@ -24,8 +26,7 @@
     wg_mtu = 1408;
 
     wg_private_key_path = "/var/lib/wireguard/private.key";
-    wg_homelab_peer_pubkey = "W2w72HnZqCouR1gRuB7LB8NgGk3pBULzxyWezXILO3c=";
-
+    wg_homelab_peer_pubkey = "wFLCjKjqIRATFeRKhNw0ESc64H0IRxN1aLvR5xSNYxU=";
     ssh_authorized_keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCfsYNHpOOrLtFd3rv8RPIl+WIDi3ePHBtffp6vmqWbY9NghnPXIUCLcuJiC845xLtOLAOLdSSn0MT2QqcPwesXfAZI5aBks2ojsNsPHB9ucoGrNRAln877RTju3iEMafUs2baSR9G8NBsy7FkitiLlRmoJlYYoLh0mPP+GB3eUldfDiXTORtRzXGfUSbLMam81WqhGn+8k+NLxmSZQEnd/HqNhlL7cy69yJqDCXcjUHbrttGqIZtoYzHq+p4GUqwBGTJmCM11PLOvtk1XlTAjZ1aAFna/A7tca9qTAIW1PJ9JSWbyIw10BXuDBY7UT565tTF8R/0zVZpu85dnH7zZn ssh-key-2025-09-03"
     ];
